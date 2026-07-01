33164c48296d97016139d338a4fec4e795edfd2e
DizzyMoon <mikkel.simons@gmail.com>
2026-07-01 03:23:24 +0200

    Changed a bunch

--- lua/irozage/configs/treesitter.lua
+++ lua/irozage/configs/treesitter.lua
@@ -1,17 +1,11 @@
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { "c", "cpp", "lua", "python", "javascript", "rust", "java"},
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  indent = {
-    enable = true
-  },
-  playground = {
-    enable = true,  -- optional
-    disable = {},
-    updatetime = 25, -- debounced time for highlighting nodes in playground from source code
-  }
-}
+local langs = { "c", "cpp", "lua", "python", "javascript", "rust", "java" }
+
+require('nvim-treesitter').install(langs)
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = langs,
+  callback = function()
+    vim.treesitter.start()
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
+})
